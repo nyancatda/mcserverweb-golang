04f7584791e7666cf8e7426b2e0e7d45016ba52a
NyanCatda <nyancatda@outlook.com>
2021-08-25 22:02:57 +0800

    完成制作

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # mcserverweb-golang
-我的世界基岩版服务器官网模板-Golang版
+我的世界基岩版服务器官网模板的Golang版本
+
+通过编辑config.json即可搭建一个我的世界服务器官网
+
+## config.json参数说明
+|参数|说明|备注|
+|----|----|----|
+|servername|服务器名字|
+|ip|服务器IP|
+|port|服务器端口|
+|introduced|服务器介绍|一句话介绍服务器，不建议太长
+|qqgrouplink|QQ群链接|这里必须填写链接，否则不会跳转
+|email|服务器邮箱|
+|serverport|网站运行端口|
+
+## 背景图片说明  
+替换图片`public/images/default.jpg`即可
---
+++ main.go
@@ -0,0 +1,107 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+	"encoding/json"
+    "net/http"
+    "github.com/gin-gonic/gin"
+)
+
+type Config struct {
+	ServerName string `json:"servername"`
+	IP string `json:"ip"`
+	Port string `json:"port"`
+	Introduced string `json:"introduced"`
+	QQGrouplink string `json:"qqgrouplink"`
+	Email string `json:"email"`
+	ServerPort int `json:"serverport"`
+}
+
+type MotdBEJson struct {
+    Status string `json:"status"`
+    IP string `json:"ip"`
+    Port string `json:"port"`
+    Motd string `json:"motd"`
+	Agreement string `json:"agreement"`
+	Version string `json:"version"`
+	Online string `json:"online"`
+	Max string `json:"max"`
+	Gamemode string `json:"gamemode"`
+	Delay int `json:"delay"`
+}
+
+//读取配置文件
+func getConfig()(Config){
+	jsonFile, err := os.Open("config.json")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer jsonFile.Close()
+	byteValue, _ := ioutil.ReadAll(jsonFile)
+	var config Config
+	json.Unmarshal([]byte(byteValue), &config)
+	return config
+}
+
+//请求MotdPE API
+//https://wiki.blackbe.xyz/api/motd.html
+func getMotdBE(ip string,port string)(MotdBEJson){
+	url := "http://motdpe.blackbe.xyz/api.php?ip="+ip+"&port="+port
+    res, err := http.Get(url)
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+        os.Exit(1)
+    }
+    body, err := ioutil.ReadAll(res.Body)
+    res.Body.Close()
+    if err != nil {
+        fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+        os.Exit(1)
+    }
+    var config MotdBEJson
+	json.Unmarshal([]byte(body), &config)
+	fmt.Println(config)
+    return config
+}
+
+
+func main() {
+    r := gin.Default()
+
+	r.Static("/public", "./public")//定义静态资源目录
+
+	r.LoadHTMLGlob("assets/**/*")
+    r.GET("/", func(c *gin.Context) {
+		Config := getConfig()
+		ServerInfo := getMotdBE(Config.IP,Config.Port)
+
+		var Status string
+		var Status_bool bool
+		if ServerInfo.Status == "online"{
+			Status = "在线"
+			Status_bool = true
+		} else {
+			Status = "离线"
+			Status_bool =false
+		}
+
+        c.HTML(http.StatusOK, "index/index.html", gin.H{
+			"servername": Config.ServerName,
+			"ip": Config.IP,
+			"port": Config.Port,
+			"introduced": Config.Introduced,
+			"qqgrouplink": Config.QQGrouplink,
+			"email": Config.Email,
+			"status": Status,
+			"status_bool": Status_bool,
+			"online":ServerInfo.Online,
+			"max":ServerInfo.Max,
+			"delay":ServerInfo.Delay,
+			"version":ServerInfo.Version})
+    })
+
+	ServerPort := fmt.Sprintf("%d", getConfig().ServerPort)
+    r.Run(":"+ServerPort)
+}
